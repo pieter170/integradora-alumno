8af3c4d4ebae90e72e2cde6b8f51b849346c77ae
pieter170 <482200115@alumnos.utzac.edu.mx>
2025-11-05 18:04:31 -0600

    first commit

---
+++ src/app/pages/pedido/pedido.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pedido',
+  templateUrl: './pedido.page.html',
+  styleUrls: ['./pedido.page.scss'],
+  standalone: false,
+})
+export class PedidoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
